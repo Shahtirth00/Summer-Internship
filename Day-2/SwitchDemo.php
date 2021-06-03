b30469e91996af543d49d322189e5527c1750c75
Shahtirth00 <85161211+Shahtirth00@users.noreply.github.com>
2021-06-03 15:56:51 +0530

    Add files via upload

---
+++ Day-2/SwitchDemo.php
@@ -0,0 +1,17 @@
+<?php
+$a = 5;
+switch ($a) {
+    case 1:
+        echo "One";
+        break;
+    case 2:
+        echo "Two";
+        break;
+    case 3:
+        echo "Three";
+        break;
+    default:
+        echo "please select between 1-3";   
+}
+?>
+
