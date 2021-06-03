e379fccb145d008bf80fd9b7b2b2ade3f5eab7c5
Shahtirth00 <85161211+Shahtirth00@users.noreply.github.com>
2021-06-03 23:13:27 +0530

    Update marksheet.php

--- Day-3/marksheet.php
+++ Day-3/marksheet.php
@@ -13,7 +13,7 @@
    <tr> <td> GENDER:   <input type="radio" name="gen" required>male
                        <input type="radio" name="gen" required>female</td></tr><br>
        <tr><td> DOB:   <input type="date" name="dob" required></td></tr><br>
-  <tr> <td> MOBILE:   <input type="number" name="mobile" max='10' required></td></tr><br>
+  <tr> <td> MOBILE:   <input type="number" name="mobile" max='9999999999' required></td></tr><br>
  
   <tr><td> MATH:    <input type="Number" name="num1" max='100' required></td></tr><br>
   <tr><td> BIOLOGY:    <input type="Number" name="num2" max='100' required></td></tr><br>
