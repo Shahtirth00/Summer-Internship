6ce77472ff0e00fbb71d24a670666da96f38438d
Shahtirth00 <85161211+Shahtirth00@users.noreply.github.com>
2021-06-03 16:23:27 +0530

    Add files via upload

---
+++ Day-3/LoopsDemo.php
@@ -0,0 +1,20 @@
+<?php
+//while loops
+    $i=0;
+    while($i<10){
+        echo "$i";
+        $i++;
+    }
+    echo "<br>";
+    //do while loop
+    $a=5;
+    do{
+        echo "$a";
+        $a++;
+    }while($a<5);
+    echo "<br>";
+    //for loops 
+    $b=12;
+    for($b=0;$b<5;$b++){
+        echo "$b";
+    }
