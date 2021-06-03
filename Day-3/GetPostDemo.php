3e4f51c2aba9a7702700af476d82c0a8017d2461
Shahtirth00 <85161211+Shahtirth00@users.noreply.github.com>
2021-06-03 16:21:49 +0530

    Add files via upload

---
+++ Day-3/GetPostDemo.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<form action="Process.php" method="GET">
+    <table>
+        <tr>
+            <td> name  </td>
+            <td><input type="text" name="txt1"/><br></td>
+        </tr>
+        <tr>
+            <td> age  </td>
+            <td> <input type="number" name="txt2"/> </td>
+        </tr>
+        <tr>
+            <td><input type="submit"/> </td>
+            <td> <input type="reset"/> </td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
---
+++ Day-3/Process.php
@@ -0,0 +1,6 @@
+<?php
+
+    $a=$GET['txt1'];
+    $b=$GET['txt2'];
+    
+    echo "my name is $a and age is $b ";
