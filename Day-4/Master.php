5786f8bd5334417ae5547eead1ccb89ecb51091e
Shahtirth00 <85161211+Shahtirth00@users.noreply.github.com>
2021-06-03 16:35:43 +0530

    Add files via upload

---
+++ Day-4/Master.php
@@ -0,0 +1,52 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Akash Technolabs</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+include './Theme/Menu.php';
+include './Theme/Logo.php';
+?>
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+		<div><img src="images/pics01.jpg" width="920" height="300" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Tittle Goes here </a></h2>
+				<p class="meta"><span class="date">June 03, 2021</span></p>
+				 
+				<div class="entry">
+                                    content goes here
+				</div>
+			</div>
+			<div class="post">
+				
+			</div>
+			
+		</div>
+		<!-- end #content -->
+		<?php
+                include './Theme/Sidebar.php';
+                ?>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+include './Theme/Footer.php';
+?>
+<!-- end #footer -->
+</body>
+</html>
