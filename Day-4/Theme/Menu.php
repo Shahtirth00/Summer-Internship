4fd6aec509613db742d6e0acb7153d4dd668e30b
Shahtirth00 <85161211+Shahtirth00@users.noreply.github.com>
2021-06-03 16:41:42 +0530

    Add files via upload

---
+++ Day-4/Theme/Menu.php
@@ -0,0 +1,10 @@
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+                    <li class="current_page_item"><a href="Home.php">Homepage</a></li>
+                        <li><a href="About.php">About</a></li>
+                        <li><a href="Contact.php">Contact</a></li>
+		</ul>
+	</div>
+	<!-- end #menu --> 
+</div>
